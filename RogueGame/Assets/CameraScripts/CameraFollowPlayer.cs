3b965733a2e70bb05d77d71e98073f1c30abe32f
UEtaylor <uraniumdriven@gmail.com>
2017-01-25 22:46:39 -0600

    Starting project

---
+++ RogueGame/Assets/CameraScripts/CameraFollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowPlayer : MonoBehaviour {
+
+	public Transform player;
+	private Vector3 PlayerPosition;
+	
+	private float PosX = -6f;
+	private float PosY = 7.25f;
+	private float PosZ = -6f;
+	private float moveSpeed = 5f;
+
+	void LateUpdate()
+	{
+		PlayerPosition = player.position + new Vector3 (PosX, PosY, PosZ);
+		transform.position = Vector3.MoveTowards(transform.position, PlayerPosition, moveSpeed);
+	}
+}
---
+++ RogueGame/Assets/PlayerScripts/PlayerClickToMove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerClickToMove : MonoBehaviour {
+
+	public float PlayerMoveSpeed;
+	public float PlayerRotationSpeed;
+	private Vector3 	targetPosition;
+	private Quaternion 	targetRotation;
+	private Transform PlayerTransform;
+ 
+void Start()
+ {
+     PlayerTransform = transform;
+     targetPosition = PlayerTransform.position;
+	 targetRotation = Quaternion.identity;
+ }
+ 
+ void Update () 
+ {                
+     //Amount to move, move speed
+     float moveAmount = PlayerMoveSpeed * Time.deltaTime;
+	 float rotateSpeed = PlayerRotationSpeed * Time.deltaTime;
+     
+     //Move the Player after they have clicked the Left Mouse Button on a location
+     if (Input.GetMouseButtonDown(0))
+     {
+         Plane playerPlane = new Plane(Vector3.up, PlayerTransform.position);
+         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+         float hitdist = 0.0f;
+         
+         if (playerPlane.Raycast(ray, out hitdist)) 
+         {
+             Vector3 targetPoint = ray.GetPoint(hitdist);
+             targetPosition = ray.GetPoint(hitdist);
+             targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+         }                        
+     }
+     transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotateSpeed);
+     PlayerTransform.position = Vector3.MoveTowards(PlayerTransform.position, targetPosition, moveAmount);
+ }
+}
